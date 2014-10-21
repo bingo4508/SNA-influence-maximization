4cc7e30386c9de235ab65d9bd0c3d0e0cbf12eaa
bingo4508 <bingo4508@gmail.com>
2014-10-21 23:19:14 +0800

    Revise lazy_greedy.py

--- run.bat
+++ run.bat
@@ -6,4 +6,4 @@ set nodes_num_per_iter=10
 set first_time_limit=300
 set time_limit=60
 
-C:\Python34\python.exe main.py %nodes_file% %edges_file% %strategy_id% %iter% %nodes_num_per_iter% %first_time_limit% %time_limit%
+C:\Python34\python.exe main_test2.py %nodes_file% %edges_file% %strategy_id% %iter% %nodes_num_per_iter% %first_time_limit% %time_limit%
